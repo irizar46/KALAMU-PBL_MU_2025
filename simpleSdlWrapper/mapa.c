21a1eda7604d421560d415d5622b6fc8d92381cf
irizar46 <kepa.irizar@alumni.mondragon.edu>
2025-01-24 10:03:44 +0100

    Delete simpleSdlWrapper directory

--- simpleSdlWrapper/mapa.c
+++
@@ -1,7 +0,0 @@
-#include "mapa.h"
-#include "graphics.h"
-#include "ourTypes.h"
-#include <imagen.h>
-#include <lib/game02.h>
-#include <stdio.h>
-#include <windows.h> // Necesario para OutputDebugString
